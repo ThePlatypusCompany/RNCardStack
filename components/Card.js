23472f484b3f3a79a43cc201f9301807afb383e2
Ridge <rto@MBAir.local>
2017-08-07 15:51:25 -0700

    create panresponder and wrap card in animated view

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,8 @@
 'use strict';
 import React, { Component } from 'react';
 import {
+  PanResponder,
+  Animated,
   View,
   Image,
   Text
@@ -8,15 +10,35 @@ import {
 import Styles from './Styles.js';
 
 export default class Card extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pan: new Animated.ValueXY()
+    };
+  }
+
+  componentWillMount() {
+    this.panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderGrant: (e, gestureState) => {},
+      onPanResponderMove: Animated.event([]),
+      onPanResponderRelease: (e, {vx, vy}) => {}
+    })
+  }
+
   render() {
     return (
-      <View style={Styles.card}>
-        <Image style={Styles.cardImage}/>
-        <View style={Styles.cardText}>
-          <Text style={Styles.cardTextMain}>Name, Age</Text>
-          <Text style={Styles.cardTextSecondary}>Job Title and Employer</Text>
+      <Animated.View {...this.panResponder.panHandlers}>
+        <View style={Styles.card}>
+          <Image style={Styles.cardImage}/>
+          <View style={Styles.cardText}>
+            <Text style={Styles.cardTextMain}>Name, Age</Text>
+            <Text style={Styles.cardTextSecondary}>Job Title and Employer</Text>
+          </View>
         </View>
-      </View>
+      </Animated.View>
     );
   }
 }
